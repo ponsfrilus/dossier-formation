3468331b42175c9bcecfd3877145dee5263eac62
Nicolas Borboën <ponsfrilus@gmail.com>
2022-08-05 09:57:28 +0200

    [fix] CSS display flex

--- generateHTML.js
+++ generateHTML.js
@@ -26,9 +26,8 @@ console.log(`
             crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <style>
-            html {
+            body {
                 width: 60%;
-                display: flex;
                 justify-content: center;
                 margin: auto;
             }
